db3f4c3a30eef831b5a3a0d4474401a54de80591
pareshvarik <varikparesh97@gmail.com>
2020-10-27 16:49:01 +0530

    word capitalize filter

---
+++ src/filters/wordCapitalize.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+export const capitalize=function(value) {
+  if (!value) return "";
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
+Vue.filter("capitalize",capitalize )
